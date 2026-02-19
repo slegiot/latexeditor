c741a698df9c039e8fa392dbf35d400310acf607
Elliot Gawn <elliot3105@gmail.com>
2026-02-19 12:54:23 +0100

    feat: Remove the entire Latex editor project

--- src/app/dashboard/loading.tsx
+++
@@ -1,5 +0,0 @@
-import { DashboardSkeleton } from "@/components/LoadingSkeleton";
-
-export default function DashboardLoading() {
-    return <DashboardSkeleton />;
-}
--- src/app/editor/[id]/loading.tsx
+++
@@ -1,5 +0,0 @@
-import { EditorSkeleton } from "@/components/LoadingSkeleton";
-
-export default function EditorLoading() {
-    return <EditorSkeleton />;
-}
